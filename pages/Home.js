b981fb7e934902c7c23acc956acee3876ca1113a
LMauANH <andrewanhle2444@gmail.com>
2025-10-10 15:21:03 +0700

    Change class names in Homepage

--- pages/Home.js
+++ pages/Home.js
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return `
-    <div class="Menu navigation bar">
+    <div class="Menu-navigation-bar">
             <div class="Homepage"></div>
             <div class="Explore"></div>
         </div>
@@ -9,18 +9,18 @@ export default function HomePage() {
         and edit as your own professional portfolio</h1>
 
     <div class="Category">
-        <div class="Category Card 1">All</div>
-        <div class="Category Card 2">Creative</div>
-        <div class="Category Card 3">Modern</div>
-        <div class="Category Card 4">Simple</div>
+        <div class="Category-Card1">All</div>
+        <div class="Category-Card2">Creative</div>
+        <div class="Category-Card3">Modern</div>
+        <div class="Category-Card4">Simple</div>
     </div>
-        <div class="Template container">
-            <div class="Template Card 1"></div>
-            <div class="Template Card 2"></div>
-            <div class="Template Card 3"></div>
-            <div class="Template Card 4"></div>
-            <div class="Template Card 5 "></div>
-            <div class="Template Card 6"></div>
+        <div class="Template-container">
+            <div class="Template-Card1"></div>
+            <div class="Template-Card2"></div>
+            <div class="Template-Card3"></div>
+            <div class="Template-Card4"></div>
+            <div class="Template-Card5"></div>
+            <div class="Template-Card6"></div>
         </div>
   `;
 
