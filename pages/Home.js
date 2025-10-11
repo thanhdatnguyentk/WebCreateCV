26385b203fa63f44ce0f1c91d4a5168832db5b79
LMauANH <andrewanhle2444@gmail.com>
2025-10-11 23:10:40 +0700

    Replace cv-template-image img element with div and change background path in css
    I would like to be able to edit a couple of stuff with this image, like
    masking it below the template items and give some border, corners, which
    is easier edditing as a css element rather than editting the whole stuff inside
    img element omg

--- pages/Home.js
+++ pages/Home.js
@@ -13,37 +13,31 @@ export default function HomePage() {
     <div class="Template-container">
             <div class="Template-Card">
               <div class="Template-Thumbnail">
-                <img src="images/mau-cv-an-tuong-1.webp" alt="CV template1" />
-              </div>
-            </div>
-            
-             <div class="Template-Card">
-              <div class="Template-Thumbnail">
-                <img src="images/mau-cv-an-tuong-1.webp" alt="CV template1" />
               </div>
             </div>
 
-             <div class="Template-Card">
+            <div class="Template-Card">
               <div class="Template-Thumbnail">
-                <img src="images/mau-cv-an-tuong-1.webp" alt="CV template1" />
               </div>
             </div>
 
-             <div class="Template-Card">
+            <div class="Template-Card">
               <div class="Template-Thumbnail">
-                <img src="images/mau-cv-an-tuong-1.webp" alt="CV template1" />
               </div>
             </div>
 
-             <div class="Template-Card">
+            <div class="Template-Card">
               <div class="Template-Thumbnail">
-                <img src="images/mau-cv-an-tuong-1.webp" alt="CV template1" />
               </div>
             </div>
 
-             <div class="Template-Card">
+            <div class="Template-Card">
               <div class="Template-Thumbnail">
-                <img src="images/mau-cv-an-tuong-1.webp" alt="CV template1" />
+              </div>
+            </div>
+
+            <div class="Template-Card">
+              <div class="Template-Thumbnail">
               </div>
             </div>
     </div>
