8413305894a0e31f21de8acbb77f5031480dba93
haile0208 <21522033@gm.uit.edu.vn>
2025-10-20 17:23:27 +0700

    Footer,Btn

--- pages/GioiThieu.js
+++ pages/GioiThieu.js
@@ -1,9 +1,15 @@
 export default function GioiThieu() {
   return `
-    <main>
+    <main style="text-align: center; padding-top: 5rem; padding-bottom: 5rem;">
       <h2>Chào mừng đến với ARTNOC</h2>
       <p>Công cụ giúp bạn tạo CV nhanh, đẹp, và dễ sử dụng.</p>
-      <a href="#/create" class="btn">Bắt đầu tạo CV</a>
+      <br>
+      <a href="#/create" class="btn-animated btn-animated-1">
+        <svg>
+          <rect x="0" y="0" fill="none" width="100%" height="100%"/>
+        </svg>
+        Bắt đầu tạo CV
+      </a>
     </main>
   `;
 }
